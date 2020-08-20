74d85ba8aacb3c3449e0ddca62068d7d6a7a79a9
Anton Voronin <auvoronin@gmail.com>
2020-08-20 10:31:08 +0200

    geomap

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,18 +9,27 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+      map.fitBounds(bounds, {
+        padding: 10,
+        maxZoom: 2000,
+        duration: 0
+      });
+    };
+
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .addTo(map);
+    });
   }
 };
 
-export { initMapbox };
-
-
-if (mapElement) {
-
-  const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(map);
-  });
-}
+export {
+  initMapbox
+};
