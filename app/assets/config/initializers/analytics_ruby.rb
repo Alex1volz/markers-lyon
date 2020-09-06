a10c1295877bef88a135e8d3f33129d1c3f5af19
Alexander Volz <alex1volz@gmail.com>
2020-09-06 14:19:58 +0200

    analytics

---
+++ app/assets/config/initializers/analytics_ruby.rb
@@ -0,0 +1,6 @@
+# require 'segment/analytics'
+
+# Analytics = Segment::Analytics.new({
+#     write_key: 'YOUR_WRITE_KEY',
+#     on_error: Proc.new { |status, msg| print msg }
+# })
--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+Analytics = Segment::Analytics.new({
+    write_key: 'YOUR_WRITE_KEY',
+    on_error: Proc.new { |status, msg| print msg }
+})
